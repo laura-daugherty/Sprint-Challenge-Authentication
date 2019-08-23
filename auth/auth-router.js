bd011f50af45f66a9f3e124cb4c8c157c2929007
Laura Daugherty <lauradaugherty6@gmail.com>
2019-08-23 08:47:34 -0700

    WIP file setup package arranging env setup

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,58 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const Auth = require('./auth_model');
+const secrets = require('../config/secrets');
+
 
 router.post('/register', (req, res) => {
-  // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+  user.password = hash;
+
+  Auth.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  let { username, password } = req.body;
+
+  Auth.findBy({ username })
+    .first() //without first() the user is returned but in an array
+    .then(user => {
+      console.log("user", user)
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user); //<<<<<<<<<<<<<
+
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token,
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
+
+function generateToken(user) {
+  const payload = {
+    // subject is normally the user's id (who/what the token describes)
+    subject: user.id, // translates unto the "sub" property on the token
+    username: user.username,
+  };
+  const options = {
+    expiresIn: '8h',
+  };
+  return jwt.sign(payload, secrets.jwtSecret, options);
+}
 
 module.exports = router;
--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,37 @@
 module.exports = {
   development: {
     client: 'sqlite3',
-    connection: { filename: './database/auth.db3' },
     useNullAsDefault: true,
+    connection: {
+      filename: './database/auth.db3',
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
     migrations: {
       directory: './database/migrations',
-      tableName: 'dbmigrations',
     },
-    seeds: { directory: './database/seeds' },
+    seeds: {
+      directory: './database/seeds',
+    },
   },
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/test.db3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+  jest: {
+    testEnvironment: "node",
+  }
 };
+
